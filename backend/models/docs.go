fa2101696d2cc0dd7ff2ada3d05fe57473d9e4df
Polwarit Watthanahemmarat <madmax168@Polwarits-MacBook-Pro.local>
2025-12-31 20:19:43 +0700

    fixed docs model more specific

--- backend/models/docs.go
+++ backend/models/docs.go
@@ -4,12 +4,14 @@ import "gorm.io/gorm"
 
 type Docs struct {
 	gorm.Model
-	Title   string `json:"title"`
-	Type    string `json:"type"`
-	Url     string `json:"url"`
-	Summary string `json:"summary"`
-	Status  string `json:"status"`
-	UserID  uint
+	Title     string `json:"title"`
+	Type      string `json:"type"`
+	Url       string `json:"url"`
+	Summary   string `json:"summary"`
+	Status    string `json:"status"`
+	UserID    uint
+	ChatID    uint
+	MessageID uint
 
 	Connect []Relationship
 }
